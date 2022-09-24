556277bc5f0120510f9ef1aabc2e66029ffeb991
MateuszZytka <109117198+MateuszZytka@users.noreply.github.com>
2022-09-23 20:24:07 -0400

    added functionailty to save button and saving text boxes to local storage

--- assets/script.js
+++ assets/script.js
@@ -34,10 +34,59 @@ function colorBlock() {
 
 colorBlock()
 
-$("#9Btn").on("click", function(){
-  let nine = $("9input").value
-  console.log(nine)
-})
+document.getElementById("9Btn").onclick=function(e){
+  e.preventDefault();
+  nine = document.getElementById("9input").value
+  localStorage.setItem("9am", nine)
+}
+
+document.getElementById("10Btn").onclick=function(e){
+  e.preventDefault();
+  ten = document.getElementById("10input").value
+  localStorage.setItem("10am", ten)
+}
+
+document.getElementById("11Btn").onclick=function(e){
+  e.preventDefault();
+  eleven = document.getElementById("11input").value
+  localStorage.setItem("11am", eleven)
+}
+
+document.getElementById("12Btn").onclick=function(e){
+  e.preventDefault();
+  twelve = document.getElementById("12input").value
+  localStorage.setItem("12pm", twelve)
+}
+
+document.getElementById("13Btn").onclick=function(e){
+  e.preventDefault();
+  thirteen = document.getElementById("13input").value
+  localStorage.setItem("1pm", thirteen)
+}
+
+document.getElementById("14Btn").onclick=function(e){
+  e.preventDefault();
+  fourteen = document.getElementById("14input").value
+  localStorage.setItem("2pm", fourteen)
+}
+
+document.getElementById("15Btn").onclick=function(e){
+  e.preventDefault();
+  fifteen = document.getElementById("15input").value
+  localStorage.setItem("3pm", fifteen)
+}
+
+document.getElementById("16Btn").onclick=function(e){
+  e.preventDefault();
+  sixteen = document.getElementById("16input").value
+  localStorage.setItem("4pm", sixteen)
+}
+
+document.getElementById("17Btn").onclick=function(e){
+  e.preventDefault();
+  seventeen = document.getElementById("17input").value
+  localStorage.setItem("5pm", seventeen)
+}
 
 
 
